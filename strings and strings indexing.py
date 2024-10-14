66d81e8f2cbaff07da0713de767b2e1193bb3786
Anika-hub <1983NALN@gmail.com>
2024-10-15 01:32:12 +0300

    Add files via upload

---
+++ strings and strings indexing.py
@@ -0,0 +1,7 @@
+example = "Papandopala"
+print(example[0])
+print(example[-1])
+print(example[6:])
+print(example[::-1])
+print(example[1::2]) #при таком коде print(example[1:11:2]), получается также верное значение.
+
